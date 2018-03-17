0db8b7472d72eb89720a585e1259732de9222c95
QwerTech <novikovpavel90@gmail.com>
2018-03-17 21:43:10 +0300

    Fix warning

--- main/src/test/java/ru/test/rest/MainTest.java
+++ main/src/test/java/ru/test/rest/MainTest.java
@@ -12,7 +12,7 @@ public class MainTest {
     public static void setup() {
         String port = System.getProperty("server.port");
         if (port == null) {
-            RestAssured.port = Integer.valueOf(80);
+            RestAssured.port = 80;
         } else {
             RestAssured.port = Integer.valueOf(port);
         }
