a39db09503f9d1cc80c751e20c5686c15091628c
sunnykote <anastasiya.dragun@gmail.com>
2018-03-23 19:17:31 +0300

    Add UItests.Search

--- main/src/test/java/org/selenide/examples/UITests.java
+++ main/src/test/java/org/selenide/examples/UITests.java
@@ -1,16 +1,16 @@
 package org.selenide.examples;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 public class UITests {
-
-    @Test
-    public void Login()  {
+@Before
+public void Login()  {
         System.setProperty ("selenide.baseUrl","https://www.last.fm/ru/");
         System.setProperty("selenide.browser", "Chrome");
         open("");
@@ -18,6 +18,15 @@ public class UITests {
         $(By.id("id_username")).setValue("sunnykote").pressEnter();
         $(By.id("id_password")).setValue("error883+");
         $(By.name("submit")).click();
+        }
+
+    @Test
+    public void Search() {
+        $(By.linkText("Поиск")).click();
+        $(By.name("q")).setValue("FKA Twings").pressEnter();
+        $(By.linkText("Исполнители"));
+        $(By.linkText("Альбомы"));
+        $(By.linkText("Композиции"));
 
 
     }
